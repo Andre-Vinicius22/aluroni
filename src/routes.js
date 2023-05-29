2897bcfd7a9e05919f4197654ee2697981c10830
Andre-Vinicius22 <vinicius.bastos.monteiro@gmail.com>
2023-05-29 12:54:47 -0300

    feat: back button add

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ import Home from "pages/Home";
 import NavMenu from "components/NavMenu";
 import DefaultPage from "components/DefaultPage";
 import About from "pages/about";
+import Footer from "components/Footer";
+import NotFound from "pages/notFound";
 
 export default function AppRouter() {
 	return (
-		<main>
+		<main className="container">
 			<Router>
 				<NavMenu />
 				<Routes>
@@ -16,7 +18,9 @@ export default function AppRouter() {
 						<Route path="menu" element={<Menu />} />
 						<Route path="about" element={<About />} />
 					</Route>
+					<Route path="*" element={<NotFound />} />
 				</Routes>
+				<Footer />
 			</Router>
 		</main>
 	);
